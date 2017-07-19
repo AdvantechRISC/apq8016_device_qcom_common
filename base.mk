6c97f5b2b6d4c7e2325cf1662bb7d3d920a50500
billy soong <billy.soong@advantech.com.tw>
2017-07-19 23:25:07 +0800

    [cts]remove camera.front and camera.flash feature

--- base.mk
+++ base.mk
@@ -775,8 +775,6 @@ PRODUCT_PACKAGES += it7260_cfg_62011E01.bin
 PRODUCT_PACKAGES += it7260_fw_050D0014.bin
 
 PRODUCT_COPY_FILES := \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
